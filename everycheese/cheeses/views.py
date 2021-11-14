cc51a288b06d6da3e695c554560ad0b642cefdf9
Aryan <59768317+Aryanx17@users.noreply.github.com>
2021-11-15 01:36:41 +0530

    'extra features'

--- everycheese/cheeses/views.py
+++ everycheese/cheeses/views.py
@@ -13,4 +13,9 @@ class CheeseDetailView(DetailView):
 class CheeseCreateView(LoginRequiredMixin, CreateView):
     fields = ['name', 'description', 'firmness',
               'country_of_origin']
+
+    def form_valid(self, form):
+        form.instance.creator = self.request.user
+        return super().form_valid(form)
+    
     model = Cheese
