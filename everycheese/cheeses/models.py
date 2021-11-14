71d6f4953f6d87d972b4d7150dcee425108095a9
Aryan <59768317+Aryanx17@users.noreply.github.com>
2021-11-14 19:12:02 +0530

    " Added add cheese  feature "

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -1,24 +1,27 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
-# from django_extensions.db.fields import AutoSlugField
 from autoslug import AutoSlugField
 from django_countries.fields import CountryField
+from django.urls import reverse
+
 
 class Cheese(TimeStampedModel):
-
     class Firmness(models.TextChoices):
-        UNSPECIFIED =  "unspecified", "Unspecified"
+        UNSPECIFIED = "unspecified", "Unspecified"
         SOFT = 'soft', "Soft"
         SEMI_SOFT = "semi-soft", "Semi-Soft"
         SEMI_HARD = "semi-hard", "Semi-Hard"
         HARD = "hard", "Hard"
-    
+
+    def get_absolute_url(self):
+        return reverse('cheeses:detail',
+        kwargs={"slug": self.slug})
+
     def __str__(self):
         return self.name
-    
 
     name = models.CharField("Name of the cheese", max_length=255)
-    slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from = "name")
+    slug = AutoSlugField("Cheese Address", unique=True, always_update=False, populate_from= "name")
     description = models.TextField("Description", blank=True)
     firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
     country_of_origin = CountryField("Country Of Origin", blank=True)
